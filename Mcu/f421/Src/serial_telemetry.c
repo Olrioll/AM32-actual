80da95bd61def587ba0dfc54f9faea8be50a9033
Oleg <75019893+Olrioll@users.noreply.github.com>
2025-07-29 18:11:19 +0300

    Fix PA14 telemetry

--- Mcu/f421/Src/serial_telemetry.c
+++ Mcu/f421/Src/serial_telemetry.c
@@ -10,21 +10,52 @@
 #include "kiss_telemetry.h"
 
 void send_telem_DMA(uint8_t bytes)
-{ // set data length and enable channel to start transfer
+{
+#ifdef USE_PA14_TELEMETRY
+    /* blocking TX on USART2 to avoid DMA channel conflict */
+    for (uint8_t i = 0; i < bytes; ++i) {
+        while (usart_flag_get(USART2, USART_TDBE_FLAG) == RESET) {
+        }
+        usart_data_transmit(USART2, aTxBuffer[i]);
+    }
+#else
+    /* set data length and enable channel to start transfer for USART1 */
     DMA1_CHANNEL2->ctrl_bit.chen = FALSE;
     DMA1_CHANNEL2->dtcnt = bytes;
     DMA1_CHANNEL2->ctrl_bit.chen = TRUE;
+#endif
 }
 
 void telem_UART_Init(void)
 {
     gpio_init_type gpio_init_struct;
 
+#ifdef USE_PA14_TELEMETRY
+    /* initialize USART2 on PA14 for telemetry (no DMA) */
+    crm_periph_clock_enable(CRM_USART2_PERIPH_CLOCK, TRUE);
+    crm_periph_clock_enable(CRM_GPIOA_PERIPH_CLOCK, TRUE);
+
+    /* configure USART2 TX pin (PA14) */
+    gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
+    gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
+    gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
+    gpio_init_struct.gpio_pins = GPIO_PINS_14;
+    gpio_init_struct.gpio_pull = GPIO_PULL_UP;
+    gpio_init(GPIOA, &gpio_init_struct);
+    gpio_pin_mux_config(GPIOA, GPIO_PINS_SOURCE14, GPIO_MUX_1);
+
+    /* configure USART2 */
+    usart_init(USART2, 115200, USART_DATA_8BITS, USART_STOP_1_BIT);
+    usart_transmitter_enable(USART2, TRUE);
+    usart_receiver_enable(USART2, TRUE);
+    usart_single_line_halfduplex_select(USART2, TRUE);
+    usart_enable(USART2, TRUE);
+#else
     crm_periph_clock_enable(CRM_USART1_PERIPH_CLOCK, TRUE);
     crm_periph_clock_enable(CRM_GPIOB_PERIPH_CLOCK, TRUE);
     crm_periph_clock_enable(CRM_DMA1_PERIPH_CLOCK, TRUE);
 
-    /* configure the usart2 tx pin */
+    /* configure USART1 TX pin (PB6) */
     gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
     gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
     gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
@@ -49,10 +80,7 @@ void telem_UART_Init(void)
     dma_init_struct.loop_mode_enable = FALSE;
     dma_init(DMA1_CHANNEL2, &dma_init_struct);
 
- //   DMA1_CHANNEL2->ctrl |= DMA_FDT_INT;
- //   DMA1_CHANNEL2->ctrl |= DMA_DTERR_INT;
-
-    /* configure usart1 param */
+    /* configure USART1 */
     usart_init(USART1, 115200, USART_DATA_8BITS, USART_STOP_1_BIT);
     usart_transmitter_enable(USART1, TRUE);
     usart_receiver_enable(USART1, TRUE);
@@ -60,5 +88,5 @@ void telem_UART_Init(void)
     usart_dma_transmitter_enable(USART1, TRUE);
     usart_enable(USART1, TRUE);
 
-    nvic_irq_enable(DMA1_Channel3_2_IRQn, 3, 0);
+#endif
 }
